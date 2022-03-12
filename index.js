2bba47d32f68a76b7a630762aa3381dd58b5d9c6
hari07 <hariodukkathil@gmail.com>
2022-03-12 17:19:33 +0530

    initialize

---
+++ index.js
@@ -0,0 +1,29 @@
+const redux = require('redux')
+const store = redux.createStore()
+
+const BUY_CAKE ='BUY_CAKE'
+
+function buyCake(){
+    return{
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+} 
+
+//
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case BUY_CAKE:{
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes -1
+            }
+        }
+        default: return state;
+    }
+}
