00786a57862bcd766368d624edc3ce4fe46f2583
hari07 <hariodukkathil@gmail.com>
2022-03-12 21:26:59 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
+
 const createStore = redux.createStore
 const combineReducer = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
+
 
 const BUY_CAKE ='BUY_CAKE'
 const BUY_PASTRY = 'BUY_PASTRY'
@@ -8,14 +14,14 @@ const BUY_PASTRY = 'BUY_PASTRY'
 function buyCake(){
     return{
         type: BUY_CAKE,
-        info: 'First redux action'
+        info: 'someone bought a cake'
     }
 } 
 
 function buyPastry(){
     return{
         type: BUY_PASTRY,
-        info: 'First redux action'
+        info: 'someone bought a pastry'
     }
 } 
 
@@ -58,9 +64,9 @@ const rootReducer = combineReducer({
 })
 
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 console.log('initial state',store.getState());
-const unsubscribe = store.subscribe(()=> console.log('dispatched State',store.getState()))
+const unsubscribe = store.subscribe(()=> {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
